a04604ee7c5aa766ec035c3f5dbc895e9a532d1b
akshay.gupta@rootinfosol.com <akshay.gupta@rootinfosol.com>
2023-01-08 21:30:54 +0530

    React Session 1

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading1 = document.createElement("h1");
+heading1.innerHTML = "Heading 1";
+
+const heading2 = document.createElement("h1");
+heading2.innerHTML = "Heading 2";
+
+const root = document.getElementById("root");
+root.appendChild(heading1);
+root.appendChild(heading2);
