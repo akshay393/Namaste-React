f915f6276a12ef9191d786ad622296607c3526f1
akshay.gupta@rootinfosol.com <akshay.gupta@rootinfosol.com>
2023-01-08 22:02:28 +0530

    React Session 1

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/*
 const heading1 = document.createElement("h1");
 heading1.innerHTML = "Heading 1";
 
@@ -7,3 +8,13 @@ heading2.innerHTML = "Heading 2";
 const root = document.getElementById("root");
 root.appendChild(heading1);
 root.appendChild(heading2);
+*/
+
+// Using React
+const heading1 = React.createElement("h1",{},"Heading 1");
+const heading2 = React.createElement("h1",{},"Heading 2");
+
+const container = React.createElement("div",{id : 'container'},[heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container);
