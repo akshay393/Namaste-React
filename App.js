c892ae5b7af757e58cff4b1f47f5c6274144cfd2
akshay.gupta@rootinfosol.com <akshay.gupta@rootinfosol.com>
2023-01-21 17:34:33 +0530

    Session-3

--- App.js
+++ App.js
@@ -13,11 +13,39 @@ root.appendChild(heading1);
 root.appendChild(heading2);
 */
 
-// Using React
-const heading1 = React.createElement("h1",{},"Heading 1");
-const heading2 = React.createElement("h1",{},"Heading 2");
+// Using React.createElement
+/*const heading1 = React.createElement("h1",{key:"1"},"Heading 1");
+const heading2 = React.createElement("h1",{key:"2"},"Heading 2");
 
 const container = React.createElement("div",{id : 'container'},[heading1, heading2]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
+root.render(container);*/
+
+// Using JSX
+const titleElement = (
+    <h1 id='title'key="1">
+      Title Element
+    </h1>
+)
+
+const TitleComponent = ()=> {
+    return (
+        <h1>Title Component</h1>
+    )
+}
+
+const HeaderComponent = ()=> {
+    return (
+        <div>
+            {titleElement}
+            {/* {TitleComponent()}     */}
+            <TitleComponent />
+            <h1>Component 1</h1>
+            <h2>Component 2</h2>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeaderComponent />)
